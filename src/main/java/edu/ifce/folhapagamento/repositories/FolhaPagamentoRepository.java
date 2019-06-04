d6554ff429f660957e142d75e9356784f72d10c6
marcus <marcusvtv@gmail.com>
2019-06-04 01:13:44 -0300

    Repositorios

---
+++ src/main/java/edu/ifce/folhapagamento/repositories/FolhaPagamentoRepository.java
@@ -0,0 +1,9 @@
+package edu.ifce.folhapagamento.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.ifce.folhapagamento.domain.FolhaPagamento;
+
+public interface FolhaPagamentoRepository extends CrudRepository<FolhaPagamento, Integer> {
+
+}
---
+++ src/main/java/edu/ifce/folhapagamento/repositories/OcorrenciaFolhaRepository.java
@@ -0,0 +1,9 @@
+package edu.ifce.folhapagamento.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.ifce.folhapagamento.domain.OcorrenciaFolha;
+
+public interface OcorrenciaFolhaRepository extends CrudRepository<OcorrenciaFolha, Integer> {
+
+}
